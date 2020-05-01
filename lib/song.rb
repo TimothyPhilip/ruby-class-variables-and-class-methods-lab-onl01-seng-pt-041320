126cdeb9f8e37384da2837f09dca0958b98484c3
Philippe Timothe <philippetimothe@gmail.com>
2020-05-01 11:45:39 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,35 @@
+require 'pry'
+class Song 
+  @@count = 0 
+  @@artists = []
+  @@genres = []
+  attr_accessor :name, :artist, :genre
+  
+  def initialize(name, artist, genre)
+    @@count += 1 
+    @@genres << genre
+    @@artists << artist
+    @name = name
+    @artist = artist
+    @genre = genre
+  end
+  
+  def self.count 
+    @@count
+  end
+  
+  def self.genres
+    @@genres.uniq
+  end
+  
+  def self.genre_count
+    @@genres.each do |genre_list|
+      
+   end 
+  end 
+  
+  def self.artists 
+    @@artists.uniq
+  end
+  
+end 
