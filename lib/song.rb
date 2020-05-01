45d4cfb592ca7b83ef10207f8cff03e3ddaf525d
Philippe Timothe <philippetimothe@gmail.com>
2020-05-01 12:25:50 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -27,7 +27,7 @@ class Song
       number = 1
     @@genres.each do |genre_list|
       if !hash.include?(genre_list)
-         hash[genre_list] << genre_list
+         hash[genre_list] << number += 1
       else
           hash[genre_list] = []
           hash[genre_list] 
